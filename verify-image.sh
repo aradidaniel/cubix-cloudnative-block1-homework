91b5704b1f443251bec73720166fe921dfdc5b86
aradidaniel <106033926+aradidaniel@users.noreply.github.com>
2024-05-31 09:56:18 +0200

    Update verify-image.sh

--- verify-image.sh
+++ verify-image.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 docker run -d --name test -p 8080:8080 ghcr.io/aradidaniel/cubix/1/homework/app:springboot > /dev/null
-sleep 10
+sleep 60
 curl --fail http://localhost:8080/cubix/test
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
